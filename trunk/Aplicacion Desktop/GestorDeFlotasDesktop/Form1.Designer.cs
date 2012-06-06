5f3eb13298763dc64f9ed526f79e1bc30c50f0c3
emiibarrola@gmail.com <emiibarrola@gmail.com@6ee224ea-d35d-a87c-4f6d-ef173ce4e3f5>
2012-06-06 03:09:16 +0000

    modificados los formularios: AbmAuto, Login, Form1, y el archivo Program

--- trunk/Aplicacion Desktop/GestorDeFlotasDesktop/Form1.Designer.cs
+++ trunk/Aplicacion Desktop/GestorDeFlotasDesktop/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace GestorDeFlotasDesktop
+﻿using System.Windows.Forms;
+namespace GestorDeFlotasDesktop
 {
     partial class Form1
     {
@@ -28,12 +29,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label1;
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            label1.AutoEllipsis = true;
+            label1.AutoSize = true;
+            label1.CausesValidation = false;
+            label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            label1.Location = new System.Drawing.Point(56, 38);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(166, 18);
+            label1.TabIndex = 0;
+            label1.Text = "Bienvenidos al Sistema!";
+            label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(57, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GESTOR DE FLOTAS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(112, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(279, 209);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(label1);
+            this.MinimumSize = new System.Drawing.Size(295, 247);
+            this.Name = "Form1";
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+
+
     }
 }
 
