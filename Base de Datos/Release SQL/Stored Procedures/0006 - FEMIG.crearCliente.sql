3bfc65e0c2b04b2a87e5d6aa2229deaaa94de6ac
emiibarrola@gmail.com <emiibarrola@gmail.com@6ee224ea-d35d-a87c-4f6d-ef173ce4e3f5>
2012-06-11 18:27:31 +0000

    stored procedures para ABM_Cliente

---
+++ Base de Datos/Release SQL/Stored Procedures/0006 - FEMIG.crearCliente.sql
@@ -0,0 +1,48 @@
+USE [GD1C2012]
+GO
+/****** Object:  StoredProcedure [FEMIG].[crearCliente]    Script Date: 06/10/2012 22:39:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/*+++++++++++++++++++++++++++++++++++++++
+* Grupo: FEMIG							*			
+*                                       *
+* Integrantes:                          *
+*	Emiliano Marcelo Ibarrola           *
+*	Marcos Andres Ibarrola              *
+*	Ignacio Angel Tata                  *
+*	Fernando N. Tobares Garcia          *
+*+++++++++++++++++++++++++++++++++++++++*/
+CREATE PROCEDURE [FEMIG].[cliente] 
+	@pDniCliente		NUMERIC(18),
+	@pNombre			VARCHAR(255),
+	@pApellido			VARCHAR(255),
+	@pTelefono			VARCHAR(18),
+	@pDireccion			VARCHAR(255),
+	@pEmail				VARCHAR(255),
+	@pFechaNacimiento 	DATETIME,
+	@pAnulado			BIT,
+	@retCatchError		VARCHAR(MAX) out
+AS
+BEGIN
+	--Controlo que no haya duplicados de Patente
+	if exists (select 1 from FEMIG.cliente where dniCliente = @dniCliente)
+	begin
+		set @retCatchError = 'Ya existe un cliente con el mismo DNI ' + @dniCliente + '.'
+		return
+	end
+
+	INSERT INTO [GD1C2012].[FEMIG].[cliente]
+           (dniCliente,nombre,apellido,telefono,direccion,email,fechaNacimiento,anulado)
+    VALUES
+           (@pDniCliente
+           ,@pNombre
+           ,@pApellido
+           ,@pTelefono
+           ,@pDireccion
+           ,@pEmail
+		   ,@pFechaNacimiento
+           ,'0')
+	
+END
---
+++ Base de Datos/Release SQL/Stored Procedures/0007 - editarCliente.sql
@@ -0,0 +1,46 @@
+USE [GD1C2012]
+GO
+/****** Object:  StoredProcedure [FEMIG].[editarCliente]    Script Date: 06/11/2012 02:10:24 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/*+++++++++++++++++++++++++++++++++++++++
+* Grupo: FEMIG							*			
+*                                       *
+* Integrantes:                          *
+*	Emiliano Marcelo Ibarrola           *
+*	Marcos Andres Ibarrola              *
+*	Ignacio Angel Tata                  *
+*	Fernando N. Tobares Garcia          *
+*+++++++++++++++++++++++++++++++++++++++*/
+CREATE PROCEDURE [FEMIG].[editarCliente] 
+	@pDniCliente		numeric(18),
+	@pNombre			VARCHAR(255),
+	@pApellido			VARCHAR(255),
+	@pTelefono			VARCHAR(18),
+	@pDireccion			VARCHAR(255),
+	@pEmail				VARCHAR(255),
+	@pFechaNacimiento 	DATETIME,
+	@pAnulado			BIT,
+	@retCatchError		VARCHAR(MAX) out
+AS
+BEGIN
+	--Controlo que no haya duplicados de Patente
+	if not exists (select 1 from FEMIG.clientes where dniCliente = @pDniCliente)
+	begin
+		set @pRetCatchError = 'No existe un cliente con el mismo DNI ' + @pDniCliente + '.'
+		return
+	end
+
+	UPDATE [GD1C2012].[FEMIG].[clientes]
+	   SET [dniCliente] = @pDniCliente
+		  ,[nombre] = @pNombre
+		  ,[apellido] = @pApellido
+		  ,[telefono] = @pTelefono
+		  ,[direccion] = @pDireccion
+		  ,[email] = @pEmail
+		  ,[fechaNacimiento] = @pFechaNacimiento
+	 WHERE dniCliente = @pDniCliente
+	
+END
