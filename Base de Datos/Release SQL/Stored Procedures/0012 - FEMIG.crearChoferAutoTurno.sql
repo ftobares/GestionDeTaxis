10b54d11b19c518bf1d91389863aa01324f08bda
emiibarrola@gmail.com <emiibarrola@gmail.com@6ee224ea-d35d-a87c-4f6d-ef173ce4e3f5>
2012-06-11 20:39:05 +0000

    SP para las relaciones Auto chofer turno

---
+++ Base de Datos/Release SQL/Stored Procedures/0012 - FEMIG.crearChoferAutoTurno.sql
@@ -0,0 +1,47 @@
+USE [GD1C2012]
+GO
+/****** Object:  StoredProcedure [FEMIG].[crearChoferAutoTurno]    Script Date: 06/10/2012 22:39:42 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/*+++++++++++++++++++++++++++++++++++++++
+* Grupo: FEMIG							*			
+*                                       *
+* Integrantes:                          *
+*	Emiliano Marcelo Ibarrola           *
+*	Marcos Andres Ibarrola              *
+*	Ignacio Angel Tata                  *
+*	Fernando N. Tobares Garcia          *
+*+++++++++++++++++++++++++++++++++++++++*/
+CREATE PROCEDURE [FEMIG].[crearChoferAutoTurno] 
+	@pFecha 			datetime,
+	@pDniChofer		 	numeric(18),
+	@pTurnoID 			numeric(20),
+	@pPatente 			varchar(10),
+	@retCatchError		VARCHAR(MAX) out
+AS
+BEGIN
+
+	--Controlo que para una fecha, no haya dos turnos iguales
+	if exists (select 1 from FEMIG.ChoferAutoTurno where fecha = @pFecha AND turnoID = @pTurnoID)
+	begin
+		set @retCatchError = 'Ya fue asignado el turno ' + cast(@pTurnoID as varchar) + ' para la fecha ' + cast(@pFecha as varchar) + '.'
+		return
+	end
+
+	--Controlo que para un mismo auto en la misma fecha y turno, no haya dos choferes
+	if exists (select 1 from FEMIG.ChoferAutoTurno where fecha = @pFecha AND turnoID = @pTurnoID AND dniChofer = @pDniChofer)
+	begin
+		set @retCatchError = 'Ya fue asignado el chofer ' + cast(@pDniChofer as varchar) + ' para la fecha ' + cast(@pFecha as varchar) + ', el turno ' + cast(@pTurnoID as varchar) + 'y el auto ' + @pPatente + '.'
+		return
+	end
+	
+	INSERT INTO [GD1C2012].[FEMIG].[ChoferAutoTurno]
+           (fecha,dniChofer,turnoID,patente)
+    VALUES
+           (@pFecha
+           ,@pDniChofer
+           ,@pTurnoID
+           ,@pPatente)
+END
