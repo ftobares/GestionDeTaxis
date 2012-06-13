502bdcf57329d85bd34d8439e14b4f747f9b1d97
emiibarrola@gmail.com <emiibarrola@gmail.com@6ee224ea-d35d-a87c-4f6d-ef173ce4e3f5>
2012-06-13 19:16:16 +0000

    Revisar

---
+++ Base de Datos/Release SQL/Stored Procedures/0024 - FEMIG.crearViaje.sql
@@ -0,0 +1,61 @@
+USE [GD1C2012]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/*+++++++++++++++++++++++++++++++++++++++
+* Grupo: FEMIG							*			
+*                                       *
+* Integrantes:                          *
+*	Emiliano Marcelo Ibarrola           *
+*	Marcos Andres Ibarrola              *
+*	Ignacio Angel Tata                  *
+*	Fernando N. Tobares Garcia          *
+*+++++++++++++++++++++++++++++++++++++++*/
+CREATE PROCEDURE [FEMIG].[crearViaje] 
+	@pTipoViaje varchar(10) NOT NULL,
+	@pDniChofer numeric(18) ,
+	@pTurnoID numeric(18) ,
+	@pCantFichas numeric(18) ,
+	@pFecha datetime ,
+	@pDniCliente numeric(18),
+	@pRetCatchError		VARCHAR(MAX) out
+AS
+DECLARE @sFecha varchar(MAX) 
+SET @sFecha = CAST(CAST(@pFecha AS date) AS varchar) + ' %'
+DECLARE @iAsignacionID numeric(18) 
+BEGIN
+
+	--Controlo si 2 Viajes para el mismo cliente a la misma hora
+	iF exists	(SELECT	1 FROM femig.Viajes
+				WHERE	 (fecha = @pFecha)
+						AND (@pDniCliente = dniCliente))
+	begin
+		set @pRetCatchError = 'El Viaje que intenta ingresar ya fue ingresado.'
+		return
+	end
+	
+	SET @iAsignacionID = SELECT TOP(1) asignacionId FROM femig.ChoferAutoTurno where (turnoID=@pTurnoID AND dniChofer = @pDniChofer AND fecha = @sFecha )
+	iF exists (@iAsignacionID)
+	begin
+		set @pRetCatchError = 'El Chofer no esta registrado para ese turno y fecha '
+		return
+	end
+	
+	IF (@pTipoViaje = 'registrado')
+	begin
+		set @dniCliente = null;
+	end
+		
+	INSERT INTO [GD1C2012].[FEMIG].[Viajes]
+           (tipoViaje,asignacionId,cantFichas,fecha,dniCliente)
+    VALUES
+           (@pTipoViaje
+           ,@iAsignacionID
+           ,@pCantFichas
+           ,@pFecha
+           ,@pDniCliente)
+	
+END
